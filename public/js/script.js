d8d9f5d8fff794324fd51f93f82618c476ff084c
Sam <samconroy@gmail.com>
2012-02-17 09:42:56 -0500

    Added theme for slider

--- public/js/script.js
+++ public/js/script.js
@@ -2,13 +2,18 @@ $(document).ready(function() {
   $("#white_chips").click(function(){
     $(this).children(".slider").show();
     $(this).children(".range").show();  
-  });
-  $("#white_chips :range");  
+  });  
   $("#chips img").draggable({
     revert: 'invalid',
 	stack: '#chips',
   });
-  $("#chip_pile").droppable();
+  $("#chip_pile").droppable( {
+    drop: functon(event, ui) {
+      var chip = $(ui.draggable);
+      console.log("chips" + chip.data("qty"));
+    }
+  });
+   
   $(".slider").slider({
     animate: true,
     range: "min",
@@ -17,14 +22,15 @@ $(document).ready(function() {
     max: $(".num_chips").text(),
     step: 1,
     slide: function(event, slider){
-      var parent = $("event.target").parent();
+      var parent = $(event.target).parent();
       parent.find(".slider-result").html(slider.value);
       var new_qty = parent.find("img").data("max") - slider.value;
-      parent.find("num_chips").text(new_qty);
+      parent.find(".num_chips").text(new_qty);
     },
     change: function(event, slider){
-      var img = $("event.target").parent().find("img");
+      var img = $(event.target).parent().find("img");
       img.data("qty", slider.value);
+      console.log("image.data" + img.data("qty"));
     }  
   });
 });
